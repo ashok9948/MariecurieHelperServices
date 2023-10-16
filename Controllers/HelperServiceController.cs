abb42f54e7909531029ea2a11b927a24f6c480bb
ashok9948 <ashok.e4m@gmail.com>
2023-10-16 18:57:25 +0100

    MariecurieHelperServices
    List that contains all Marie Curie Helper Services
    See whether the helpline is OPEN or CLOSED

---
+++ Controllers/HelperServiceController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InterviewTask.Models;
+
+namespace InterviewTask.Controllers
+{
+    public class HelperServiceController : Controller
+    {
+        // GET: HelperService
+       
+        public ActionResult Index()
+        {
+            // Assuming you have a list of HelperService objects
+            List<HelperServiceModel> helperServices = GetHelperServices();
+
+            return View(helperServices);
+        }
+        private List<HelperServiceModel> GetHelperServices()
+        {
+            // Retrieve data from a database or another source
+            // For simplicity, creating dummy data
+            return new List<HelperServiceModel>
+        {
+            new HelperServiceModel
+        {
+            Id = Guid.NewGuid(),
+            Title = "Service 1",
+            Description = "Description for Service 1 - We know the little things can make a big difference when you, or someone you care about, are living with a terminal illness.",
+            TelephoneNumber = "123-456-7890",
+            MondayOpeningHours = new List<int> { 9, 17 }, // Opening hours for Monday, e.g., 9 AM to 5 PM
+            TuesdayOpeningHours = new List<int> { 9, 17 },
+            WednesdayOpeningHours = new List<int> { 9, 17 },
+            ThursdayOpeningHours = new List<int> { 9, 17 },
+            FridayOpeningHours = new List<int> { 9, 17 },
+            SaturdayOpeningHours = new List<int> { 10, 15 }, // Adjusted hours for Saturday
+            SundayOpeningHours = new List<int> { 12, 16 } // Adjusted hours for Sunday
+        },
+            new HelperServiceModel
+        {
+            Id = Guid.NewGuid(),
+            Title = "Service 2",
+            Description = "Description for Service 2 - We know the little things can make a big difference when you, or someone you care about, are living with a terminal illness.",
+            TelephoneNumber = "123-456-7890",
+            MondayOpeningHours = new List<int> { 9, 17 }, // Opening hours for Monday, e.g., 9 AM to 5 PM
+            TuesdayOpeningHours = new List<int> { 9, 17 },
+            WednesdayOpeningHours = new List<int> { 9, 17 },
+            ThursdayOpeningHours = new List<int> { 9, 17 },
+            FridayOpeningHours = new List<int> { 9, 17 },
+            SaturdayOpeningHours = new List<int> { 10, 15 }, // Adjusted hours for Saturday
+            SundayOpeningHours = new List<int> { 12, 16 } // Adjusted hours for Sunday
+        }
+
+        };
+        }
+
+    }
+}
---
+++ Models/HelperServiceModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewTask.Models
+{
+    public class HelperServiceModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string TelephoneNumber { get; set; }
+
+        public List<int> MondayOpeningHours { get; set; }
+        public List<int> TuesdayOpeningHours { get; set; }
+        public List<int> WednesdayOpeningHours { get; set; }
+        public List<int> ThursdayOpeningHours { get; set; }
+        public List<int> FridayOpeningHours { get; set; }
+        public List<int> SaturdayOpeningHours { get; set; }
+        public List<int> SundayOpeningHours { get; set; }
+
+        public string GetClosingTime()
+        {
+            // Assuming the closing time is the second element in the list
+            return $"{SundayOpeningHours[1]}:00"; // You might want to adjust this based on your actual data
+        }
+
+        public string GetOpeningTime()
+        {
+            // Assuming the opening time is the first element in the list
+            return $"{MondayOpeningHours[0]}:00"; // You might want to adjust this based on your actual data
+        }
+
+        public string NextOpenDay
+        {
+            get
+            {
+                var now = DateTime.Now;
+                var daysUntilNextOpen = ((int)now.DayOfWeek - 1 + 7) % 7; // Calculate days until the next Monday
+
+                // Find the next open day
+                for (int i = 0; i < 7; i++)
+                {
+                    int nextDay = (int)(now.DayOfWeek + i) % 7;
+                    if (IsOpenOnDay(nextDay))
+                    {
+                        return GetDayOfWeekName(nextDay+1);
+                    }
+                }
+
+                return "Unknown";
+            }
+        }
+
+        public bool IsOpenOnDay(int day)
+        {
+            switch (day)
+            {
+                case 0:
+                    return SundayOpeningHours.Count > 0;
+                case 1:
+                    return MondayOpeningHours.Count > 0;
+                case 2:
+                    return TuesdayOpeningHours.Count > 0;
+                case 3:
+                    return WednesdayOpeningHours.Count > 0;
+                case 4:
+                    return ThursdayOpeningHours.Count > 0;
+                case 5:
+                    return FridayOpeningHours.Count > 0;
+                case 6:
+                    return SaturdayOpeningHours.Count > 0;
+                default:
+                    return false;
+            }
+        }
+
+        public string GetDayOfWeekName(int day)
+        {
+            return Enum.GetName(typeof(DayOfWeek), day);
+        }
+    }
+
+}
+
